c1ff4cae780841d07e5ec332cd45a74dadeb313a
Vishwas87 <efiengo@gmail.com>
2013-10-17 12:25:42 +0200

    New
    Creazione App

---
+++ cmp_login/cmp_login/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cmp_login
+//
+//  Created by Vincenzo on 17/10/13.
+//  Copyright (c) 2013 Vincenzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ cmp_login/cmp_login/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  cmp_login
+//
+//  Created by Vincenzo on 17/10/13.
+//  Copyright (c) 2013 Vincenzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
